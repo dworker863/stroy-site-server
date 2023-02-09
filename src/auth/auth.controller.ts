4d627b2d93e33cd3e25e19563dfe10bae3d75fe2
dworker <dworker863@gmail.com>
2023-02-09 22:27:41 +0600

    Added auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local.guard';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 }
